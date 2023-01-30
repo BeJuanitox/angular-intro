9f7665fc462ce964d9deed22221d2728884acbc2
BeJuanitox <jsebastian23@hotmail.com>
2023-01-30 16:40:35 -0500

    Destructuring fuctions

---
+++ src/Ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,38 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Huawei',
+    price: 1200000
+}
+
+const tablet: Product = {
+    description: 'Ipad',
+    price: 2000000
+}
+
+function calculaISV(products: Product[] ): [number, number] {
+    let total = 0;
+
+    //products.forEach( (product) => {
+    products.forEach( ({price}) => {
+        //total += product.price;
+        total += price;
+    }) 
+
+    return [total, total * 0.15];
+}
+
+const articles = [phone, tablet];
+
+const [total, isv] = calculaISV(articles);
+
+console.log('Total: ', total)
+console.log('ISV: ', isv)
